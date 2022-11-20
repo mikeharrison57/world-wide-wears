c5f992a8c1d0bda9695b2c651a7b1c5672012470
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-11-20 16:19:38 -0700

    Set up intial API call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+// Components
 import Navbar from "./components/Navbar/Navbar";
 import Banner from "./components/Banner/Banner";
 
+// Functions and Hooks
+import { useState, useEffect } from 'react'; 
+import { fetchSaleItems } from "./utils/api-calls";
+
 const App = () => {
+
+  
+
   return (
     <main>
       <Navbar />
