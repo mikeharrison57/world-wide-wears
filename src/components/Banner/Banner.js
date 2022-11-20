2133412faa8ffdd96b8982a7cf5449071de182f4
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-11-20 15:33:41 -0700

    Set up and render Banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import BlackFriday from '../../assets/black-friday.jpg';
+import Clothes from '../../assets/clothes.jpg';
+
+const Banner = () => {
+	return (
+    <section>
+      <img alt='black-friday' src={BlackFriday} />
+      <img alt='clothes' src={Clothes} />
+      <img alt='black-friday' src={BlackFriday} />
+    </section>
+  )
+};
+
+export default Banner;
