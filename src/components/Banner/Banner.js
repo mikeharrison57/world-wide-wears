e989ebc56b513b30f2048c00ced01a3bbd5598ee
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-11-20 15:40:58 -0700

    Basic styling for banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,14 +1,15 @@
 import BlackFriday from '../../assets/black-friday.jpg';
 import Clothes from '../../assets/clothes.jpg';
+import './Banner.css'
 
 const Banner = () => {
 	return (
-    <section>
-      <img alt='black-friday' src={BlackFriday} />
-      <img alt='clothes' src={Clothes} />
-      <img alt='black-friday' src={BlackFriday} />
-    </section>
-  )
+		<section>
+			<img alt='black-friday' src={BlackFriday} className='banner-image' />
+			<img alt='clothes' src={Clothes} className='banner-image' />
+			<img alt='black-friday' src={BlackFriday} className='banner-image' />
+		</section>
+	);
 };
 
 export default Banner;
