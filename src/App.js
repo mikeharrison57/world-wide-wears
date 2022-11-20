ba39320a4c692100c3dfddd2b5a1ac29ced899ef
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-11-20 16:26:34 -0700

    Set up Error component and useState hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 // Components
 import Navbar from "./components/Navbar/Navbar";
 import Banner from "./components/Banner/Banner";
+import Error from "./components/Error/Error";
 
 // Functions and Hooks
-import { useState, useEffect } from 'react'; 
+import { useState, useEffect } from "react"; 
 import { fetchSaleItems } from "./utils/api-calls";
 
 const App = () => {
-
-  
+  const [products, setProducst] = useState([]);
+  const [error, setError] = useState(false);
 
   return (
     <main>
@@ -16,6 +17,6 @@ const App = () => {
       <Banner />
     </main>
   );
-}
+};
 
 export default App;
---
+++ src/components/Error/Error.js
@@ -0,0 +1,10 @@
+const Error = () => {
+	return (
+		<h1>
+			We're sorry, we're having some technical difficulties right now please
+			come back later
+		</h1>
+	);
+};
+
+export default Error;
