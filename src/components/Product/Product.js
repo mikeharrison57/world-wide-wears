522da9b20b4ff002ca32402ea8c51e86b7808e89
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-11-20 19:23:51 -0700

    Styled Products, particularly prices

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,13 +1,16 @@
+import './Product.css';
+
 const Product = ({ product }) => {
-  return (
-  <article>
-		{console.log(product)}
-		<img alt={product.name} src={product.thumbnailImageUrl} />
-		<h3>{product.name}</h3>
-		<p>{product.msrp}</p>
-		<p>{product.price}</p>
-	</article>
-  )
-}
+	return (
+		<article className='product-card'>
+			<img alt={product.name} src={product.thumbnailImageUrl} />
+			<h3 className='product-name'>{product.name}</h3>
+			<div className='prices'>
+				<p className='current-price'>${product.price}</p>
+				<del className='previous-price'>${product.msrp}</del>
+			</div>
+		</article>
+	);
+};
 
 export default Product;
