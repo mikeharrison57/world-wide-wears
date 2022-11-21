3dd96c12d471eb736202f8977e4e1b3f4fcb867d
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-11-20 18:21:49 -0700

    Settting API data to products

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 // Components
-import Navbar from "./components/Navbar/Navbar";
-import Banner from "./components/Banner/Banner";
-import Error from "./components/Error/Error";
+import Navbar from './components/Navbar/Navbar';
+import Banner from './components/Banner/Banner';
+import Error from './components/Error/Error';
 
 // Functions and Hooks
-import { useState, useEffect } from "react"; 
-import { fetchSaleItems } from "./utils/api-calls";
+import { useState, useEffect } from 'react';
+import { fetchSaleItems } from './utils/api-calls';
 
 const App = () => {
-  const [products, setProducst] = useState([]);
-  const [error, setError] = useState(false);
+	const [products, setProducts] = useState([]);
+	const [error, setError] = useState(false);
 
-  return (
-    <main>
-      <Navbar />
-      <Banner />
-    </main>
-  );
+	const getSaleItems = async () => {
+		fetchSaleItems()
+			.then((data) => {
+				setProducts([...products, ...data.results]);
+			})
+			.catch((error) => {
+				console.log(error);
+				setError(true);
+			});
+	};
+
+	useEffect(() => {
+		getSaleItems();
+	}, []);
+
+	return (
+		<main>
+			<Navbar />
+			<Banner />
+		</main>
+	);
 };
 
 export default App;
