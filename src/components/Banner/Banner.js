dc097d960ed9d492eee4810056f3ca7925c9e361
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-11-26 22:04:40 -0700

    Final mobile and styling tweaks

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
 	return (
 		<section className='banner-container'>
 			<img alt='banner-img' src={BannerImg} className='banner-image' />
-			<h2>Check Out Our Black Friday Deals Below</h2>
+			<h2>Check Out Our Black Friday Deals Below!</h2>
 		</section>
 	)
 };
--- src/components/CartProduct/CartProduct.js
+++ src/components/CartProduct/CartProduct.js
@@ -17,12 +17,12 @@ const CartProduct = ({ product, removeCartProduct }) => {
 			)}
 			<div className='prices-and-name'>
 				<h3 className='product-name'>{product.name}</h3>
-					<button className='remove-from-cart' onClick={confirmCartRemoval}>Remove From Cart</button>
+				<button className='remove-from-cart' onClick={confirmCartRemoval}>Remove From Cart</button>
 				<div className='prices'>
 					<p className='current-price'>${product.price}</p>
 					{product.msrp && (
 						<del className='previous-price'>${product.msrp}</del>
-					)}
+						)}
 				</div>
 			</div>
 		</article>
